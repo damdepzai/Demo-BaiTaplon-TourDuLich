ea240dfe9fbeb46de5a85138a2303d432feaba17
= <=>
2019-10-22 10:43:01 +0700

    first commit

---
+++ 1.js
@@ -0,0 +1,26 @@
+﻿ $(function(){
+	 thoigian = setInterval(function(){
+	 	$('.red').toggleClass('chuyenhieuung');
+	 },500);
+
+	 
+	 $(window).scroll(function() {
+		vitri=$('html').scrollTop()
+		console.log(vitri);
+		if(vitri>100)
+	 {
+	 	$('.back-to-top').addClass('hienra');
+	 }
+	 else
+	 {
+	 	$('.back-to-top').removeClass('hienra');
+	 }
+
+	});
+	 $('.back-to-top').click(function(event) {
+ 		$('html').animate({scrollTop:0},4000,"easeOutBounce");
+ 		return false;
+ 	});
+	 
+})  
+ 
